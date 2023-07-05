c2d12ae9f97671dec977d0d2b1a4da3e90cab9ab
Axel Jacobo <jacobaxelth@outlook.com>
2023-07-04 20:33:23 -0600

    Add slideshow functionality
    See: #13

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import SkillsCollection from "/js/classes/SkillsCollection.js";
-import glide from "/js/classes/ContainerGlidder.js";
+import glide from "/js/classes/ContainerGlider.js";
 
 const collection = new SkillsCollection(document.querySelectorAll('.skills > .container > *'));
 collection.setContainer(collection.collection[0].parentNode);
@@ -7,3 +7,77 @@ collection.setPosAll();
 collection.setShowTime();
 
 const collectionContainer = glide('.skills > .container');
+const portfolioNode =  document.querySelector('section.portfolio');
+const Portfolio = {
+  current: 1,
+  isActive: false,
+  setCurrent(i) {
+    this.isActive = true;
+    if ( i > this.current ) { 
+      this.targetWebsite = this.websites[i - 1];
+      this.current = i;
+      this.activeWebsite.style.transitionProperty = 'transform';
+      this.targetWebsite.style.transitionProperty = 'transform';
+      this.activeWebsite.addEventListener('transitionend', this.websiteTransitionHandler);
+      this.activeWebsite.classList.add('left');
+      this.targetWebsite.classList.add('active');
+      
+      if ( this.targetWebsite.classList.contains('left') ) {
+        this.targetWebsite.classList.remove('left');
+        
+      } else if ( this.targetWebsite.classList.contains('right') ) {
+        this.targetWebsite.classList.remove('right');
+      }
+      
+    }
+    else {
+      this.targetWebsite = this.websites[i - 1];
+      this.current = i;
+      this.activeWebsite.style.transitionProperty = 'transform';
+      this.targetWebsite.style.transitionProperty = 'transform';
+      this.activeWebsite.addEventListener('transitionend', this.websiteTransitionHandler);
+      this.activeWebsite.classList.add('right');
+      this.targetWebsite.classList.add('active');
+      
+      if ( this.targetWebsite.classList.contains('left') ) {
+        this.targetWebsite.classList.remove('left');
+        console.log('else -> if()')
+  
+      } else if ( this.targetWebsite.classList.contains('right') ) {
+        this.targetWebsite.classList.remove('right');
+        this.targetWebsite.classList.add('left');
+        this.targetWebsite.classList.remove('left');
+        
+      }
+      
+    }
+
+  },
+  controlsParent: portfolioNode.querySelector('.controls'),
+  controls: portfolioNode.querySelectorAll('.controls > li'),
+  websites: portfolioNode.querySelectorAll('.websites-container > .website'),
+  targetWebsite: null, 
+  activeWebsite: portfolioNode.querySelector('.website.active'),
+  websiteTransitionHandler(e) {
+    Portfolio.isActive = false;
+    Portfolio.activeWebsite.removeEventListener('transitionend', Portfolio.websiteTransitionHandler);
+    Portfolio.activeWebsite.classList.remove('active');
+    Portfolio.activeWebsite = Portfolio.targetWebsite;
+    Portfolio.targetWebsite.style.transitionProperty = 'transform';
+    Portfolio.activeWebsite.style.transitionProperty = 'transform';
+  } 
+};
+
+// Click listener for each control element
+Portfolio.controls.forEach((item) => item.addEventListener('click', (e) => {
+  if ( Portfolio.isActive ) return;
+  if ( e.target.matches('li') &&
+      !e.target.classList.contains('active') ) 
+  { 
+    let index = Array.from(Portfolio.controls).findIndex((item, i) => item === e.currentTarget);
+    Portfolio.setCurrent(index + 1);
+    Portfolio.controlsParent.querySelector('li.active')
+    .classList.remove('active');
+    e.target.classList.add('active');
+  }
+}));
